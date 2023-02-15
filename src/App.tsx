59f4166c62fb7d09a2f99796be32a1321153fe9b
Yudi Supriyadi <ysupriyadi@paystone.com>
2023-02-16 00:13:04 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,54 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleDateMasking(e: React.KeyboardEvent<HTMLInputElement>): void {
+    const select_all = e.key === 'a' && e.metaKey;
+    const only_number = '1234567890'.split('').includes(e.key);
+    const valid_keys = [
+        'ArrowLeft',
+        'ArrowRight',
+        'Backspace',
+        'Enter',
+        'Tab',
+        '/',
+    ].includes(e.key);
+
+    if (!valid_keys && !select_all && !only_number) {
+        e.preventDefault();
+
+        return;
+    }
+
+    if (e.key === 'Enter') {
+        e.preventDefault();
+
+        return;
+    }
+
+    const element = e.target as HTMLInputElement;
+    const len = element.value.length;
+
+    const SLASH_CHAR = 47;
+    if (e.keyCode == SLASH_CHAR) {
+        e.preventDefault();
+    }
+
+    const not_number = isNaN(parseInt(e.key));
+    if (not_number) {
+        return;
+    }
+
+    if (len === 2) {
+        element.value += '/';
+    }
+    if (len === 5) {
+        element.value += '/';
+    }
+}
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <input type="text" onKeyDown={handleDateMasking} />
     </div>
   )
 }
