83d4035c233755d56c2905a7e836dfe289c9f318
Yudi Supriyadi <ysupriyadi@paystone.com>
2023-03-02 20:09:36 +0700

    Inside app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next"
 
 export default function App() {
     const { t } = useTranslation();
 
+    const [is_login, setIsLogin] = useState(false)
+
     return (
         <div>
             {t('Welcome to React')}
+            <button onClick={() => setIsLogin(true)}>Login</button>
+
+            {is_login && <InsideApp />}
+        </div>
+    )
+}
+
+function InsideApp() {
+    const { t } = useTranslation();
+    return (
+        <div>
+            <h1>{t('inside.app')}</h1>
         </div>
     )
 }
