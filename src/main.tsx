9f25e6d475cd014b5b3c7c1e1dfa90824ed21527
Yudi Supriyadi <ysupriyadi@paystone.com>
2023-03-02 20:02:36 +0700

    projectId

--- src/main.tsx
+++ src/main.tsx
@@ -25,6 +25,10 @@ i18n.use(initReactI18next) // passes i18n down to react-i18next
     lng: 'en', // if you're using a language detector, do not define the lng option
     fallbackLng: 'en',
 
+    backend: {
+      projectId: '7da49584-db41-4cc7-b372-90688243eb29',
+    },
+
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
