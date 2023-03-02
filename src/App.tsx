14c7e956c05c25c78df0f6116c90fbbaca304e43
Yudi Supriyadi <ysupriyadi@paystone.com>
2023-03-02 19:48:08 +0700

    i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,11 @@
-import { useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useTranslation } from "react-i18next"
 
-function App() {
-  const ref = useRef<HTMLInputElement>(null);
+export default function App() {
+    const { t } = useTranslation();
 
-  function handleDateMasking(e: React.KeyboardEvent<HTMLInputElement>): void {
-    const select_all = e.key === 'a' && e.metaKey;
-    const only_number = '1234567890'.split('').includes(e.key);
-    const valid_keys = [
-        'ArrowLeft',
-        'ArrowRight',
-        'Backspace',
-        'Enter',
-        'Tab',
-        '/',
-    ].includes(e.key);
-
-    alert(e.key + ' ' + e.keyCode + ' ' + e.metaKey + ' ' + (e.target as HTMLInputElement).value);
-
-    if (!valid_keys && !select_all && !only_number) {
-        e.preventDefault();
-
-        return;
-    }
-
-    if (e.key === 'Enter') {
-        e.preventDefault();
-
-        return;
-    }
-
-    const element = e.target as HTMLInputElement;
-    const len = element.value.length;
-
-    const SLASH_CHAR = 47;
-    if (e.keyCode == SLASH_CHAR) {
-        e.preventDefault();
-    }
-
-    const not_number = isNaN(parseInt(e.key));
-    if (not_number) {
-        return;
-    }
-
-    if (!ref.current) {
-      return;
-    }
-
-    if (len === 2) {
-      ref.current.value += '/';
-    }
-    if (len === 5) {
-      ref.current.value += '/';
-    }
+    return (
+        <div>
+            {t('Welcome to React')}
+        </div>
+    )
 }
-
-  return (
-    <div>
-      <input type="text" onKeyDown={handleDateMasking} ref={ref} />
-    </div>
-  )
-}
-
-export default App
